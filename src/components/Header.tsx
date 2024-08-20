6fdbeb6b74fd8fc6cf414d774d2c04734af9da0a
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-08-20 19:01:37 +0530

    fix bug in video

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,15 +12,15 @@ const Header = () => {
                 Home
               </Link>
               <Link href="/image-transform" className="text-white font-medium hover:text-gray-300">
-                Image-Format
+                Image<span className="sm:inline hidden">-Format</span>
               </Link>
               <Link href="/Video-upload" className="text-white font-medium hover:text-gray-300">
-                Video-Compress
+                Video <span className="sm:inline hidden">-Compressor</span>
               </Link>
             </nav>) :(
                <nav className='flex gap-x-10 justify-between'>
                  <div className='sm:flex justify-center hidden items-center font-semibold'>Please Ignore the UI just like my career.</div>
-                 <Link href={"/sign-in"} className='bg-blue-500 p-3 hover:bg-blue-700 rounded-xl hover:shadow-xl ease-in-out duration-300 transition-all'>Login Here</Link>
+                 <Link href={"/sign-up"} className='bg-blue-500 p-3 hover:bg-blue-700 rounded-xl hover:shadow-xl ease-in-out duration-300 transition-all'>Login Here</Link>
                </nav>
             )
       }
